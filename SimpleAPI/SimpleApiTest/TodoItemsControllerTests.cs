c27c26b7379214707bdb66586dd8e189f1da001b
Chris Beaver <chrisbvr@gmail.com>
2017-06-05 19:21:21 +1000

    added patch and delete tests

--- SimpleAPI/SimpleApiTest/TodoItemsControllerTests.cs
+++ SimpleAPI/SimpleApiTest/TodoItemsControllerTests.cs
@@ -51,7 +51,7 @@ namespace SimpleApiTest
             var mockToDoItems = new Mock<DbSet<TodoItem>>();
 
             var controller = new TodoItemsController(mockApiContext.Object);
-            BadRequestResult result = (BadRequestResult)controller.Post(null);
+            StatusCodeResult result = (StatusCodeResult)controller.Post(null);
 
             Assert.AreEqual(400, result.StatusCode);
         }
@@ -64,7 +64,7 @@ namespace SimpleApiTest
             var mockToDoItems = new Mock<DbSet<TodoItem>>();
 
             var controller = new TodoItemsController(mockApiContext.Object);
-            BadRequestResult result = (BadRequestResult)controller.Put(1, null);
+            StatusCodeResult result = (StatusCodeResult)controller.Put(1, null);
             Assert.AreEqual(400, result.StatusCode);
         }
         [TestMethod]
@@ -85,13 +85,13 @@ namespace SimpleApiTest
             mockApiContext.Setup(m => m.TodoItems).Returns(mockToDoItems.Object);
 
             var controller = new TodoItemsController(mockApiContext.Object);
-            NotFoundResult result = (NotFoundResult)controller.Put(1, new TodoItem() { Id = 1, Description = "test", IsDone = true, Title = "test" });
+            StatusCodeResult result = (StatusCodeResult)controller.Put(1, new TodoItem() { Id = 1, Description = "test", IsDone = true, Title = "test" });
             Assert.AreEqual(404, result.StatusCode);
 
         }
 
         [TestMethod]
-        public void Put_TodoItem__Return_304()
+        public void Put_TodoItem_Return_204()
         {
             var mockApiContext = new Mock<ApiContext>();
             var mockToDoItems = new Mock<DbSet<TodoItem>>();
@@ -108,14 +108,135 @@ namespace SimpleApiTest
             mockApiContext.Setup(m => m.TodoItems).Returns(mockToDoItems.Object);
 
             var controller = new TodoItemsController(mockApiContext.Object);
-            NoContentResult result = (NoContentResult)controller.Put(1, new TodoItem() { Id = 1, Description = "test", IsDone = true, Title = "test" });
-            Assert.AreEqual(204,result.StatusCode);
+            StatusCodeResult result = (StatusCodeResult)controller.Put(1, new TodoItem() { Id = 1, Description = "test", IsDone = true, Title = "test" });
+            Assert.AreEqual(204, result.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void Patch_Null_Record_Return_400()
+        {
+            var mockApiContext = new Mock<ApiContext>();
+            var mockToDoItems = new Mock<DbSet<TodoItem>>();
+
+            var controller = new TodoItemsController(mockApiContext.Object);
+            StatusCodeResult result = (StatusCodeResult)controller.Patch(1, null);
+            Assert.AreEqual(400, result.StatusCode);
+        }
+        [TestMethod]
+        public void Patch_NoRecordFound_Return_404()
+        {
+            var mockApiContext = new Mock<ApiContext>();
+            var mockToDoItems = new Mock<DbSet<TodoItem>>();
+            var items = new List<TodoItem>
+            {
+                new TodoItem()
+            }.AsQueryable();
+
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Provider).Returns(items.Provider);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Expression).Returns(items.Expression);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.ElementType).Returns(items.ElementType);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.GetEnumerator()).Returns(items.GetEnumerator());
+
+            mockApiContext.Setup(m => m.TodoItems).Returns(mockToDoItems.Object);
+
+            var controller = new TodoItemsController(mockApiContext.Object);
+            StatusCodeResult result = (StatusCodeResult)controller.Patch(1, new TodoItem() { Id = 1, Description = "test", IsDone = true, Title = "test" });
+            Assert.AreEqual(404, result.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void Patch_TodoItem_Return_204()
+        {
+            var mockApiContext = new Mock<ApiContext>();
+            var mockToDoItems = new Mock<DbSet<TodoItem>>();
+            var items = new List<TodoItem>
+            {
+                new TodoItem() {Id = 1}
+            }.AsQueryable();
+
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Provider).Returns(items.Provider);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Expression).Returns(items.Expression);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.ElementType).Returns(items.ElementType);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.GetEnumerator()).Returns(items.GetEnumerator());
+
+            mockApiContext.Setup(m => m.TodoItems).Returns(mockToDoItems.Object);
+
+            var controller = new TodoItemsController(mockApiContext.Object);
+            StatusCodeResult result = (StatusCodeResult)controller.Patch(1, new TodoItem() { IsDone = true});
+            Assert.AreEqual(204, result.StatusCode);
 
         }
 
 
+        [TestMethod]
+        public void Delete_Success_Return_204()
+        {
+            var mockApiContext = new Mock<ApiContext>();
+            var mockToDoItems = new Mock<DbSet<TodoItem>>();
+            var items = new List<TodoItem>
+            {
+                new TodoItem() {Id = 1}
+            }.AsQueryable();
 
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Provider).Returns(items.Provider);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Expression).Returns(items.Expression);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.ElementType).Returns(items.ElementType);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.GetEnumerator()).Returns(items.GetEnumerator());
 
+            mockApiContext.Setup(m => m.TodoItems).Returns(mockToDoItems.Object);
+
+            var controller = new TodoItemsController(mockApiContext.Object);
+            StatusCodeResult result = (StatusCodeResult)controller.Delete(1);
+
+            Assert.AreEqual(204, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void Delete_No_Record_Return_404()
+        {
+            var mockApiContext = new Mock<ApiContext>();
+            var mockToDoItems = new Mock<DbSet<TodoItem>>();
+            var items = new List<TodoItem>
+            {
+                new TodoItem() {Id = 1}
+            }.AsQueryable();
+
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Provider).Returns(items.Provider);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Expression).Returns(items.Expression);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.ElementType).Returns(items.ElementType);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.GetEnumerator()).Returns(items.GetEnumerator());
+
+            mockApiContext.Setup(m => m.TodoItems).Returns(mockToDoItems.Object);
+
+            var controller = new TodoItemsController(mockApiContext.Object);
+            StatusCodeResult result = (StatusCodeResult)controller.Delete(2);
+
+            Assert.AreEqual(404, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void Delete_Null_Record_Return_404()
+        {
+            var mockApiContext = new Mock<ApiContext>();
+            var mockToDoItems = new Mock<DbSet<TodoItem>>();
+            var items = new List<TodoItem>
+            {
+            }.AsQueryable();
+
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Provider).Returns(items.Provider);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.Expression).Returns(items.Expression);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.ElementType).Returns(items.ElementType);
+            mockToDoItems.As<IQueryable<TodoItem>>().Setup(m => m.GetEnumerator()).Returns(items.GetEnumerator());
+
+            mockApiContext.Setup(m => m.TodoItems).Returns(mockToDoItems.Object);
+
+            var controller = new TodoItemsController(mockApiContext.Object);
+            StatusCodeResult result = (StatusCodeResult)controller.Delete(1);
+
+            Assert.AreEqual(404, result.StatusCode);
+        }
 
 
     }
